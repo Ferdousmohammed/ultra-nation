c232f5ff99d1fea4a02a2fc1fa9a3f9cf35ac439
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-06-06 21:01:19 +0600

    final-commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Country from './components/Country/Country';
 function App() {
 const [contries, setContries] = useState([])
 
+
+const [cart, setCart] = useState([])
+
   //  faching data
  useEffect (()=> {
   fetch('https://restcountries.com/v3.1/all')
@@ -17,14 +20,20 @@ const [contries, setContries] = useState([])
 
     .catch(error => console.log(error))
  }, [])
-
+ //setting a button 
+   const handleAddButton = (country)=>{ 
+    const newCart = [...cart, country]
+     setCart(newCart);
+   };
+   
   return (
     <div className="App">
       <header className="App-header">
           <p>this is my ultra nation rest api: {contries.length} </p>
+          <p>Country Added :{cart.length} </p>
          
             {
-            contries.map(country => <Country country = {country}> </Country>)
+            contries.map(country => <Country country = {country} handleAddButton = {handleAddButton}> </Country>)
             }
         
           
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Country = (props) => {
-    const {population, region, capital,flag} = props.country
+    const {population, region, capital,flag} = props.country;
+    const handleAddButton = props.handleAddButton;
     return (
-        <div>
-            <h1 style={{width: '80px', height: '40px'}}>  {flag} </h1>
+        <div style={{border: '1px solid red ', width: '250px', padding: '10px',margin: '5px', borderRadius: '12px'}}>
+            <h1 style={{}}>  {flag} </h1>
             <p> Population{population} </p>
             <p>Region{region} </p>
             <p>capital:{capital} </p>
+            <button onClick={()=> handleAddButton (props.handleAddButton) }>Add Country</button>
            
 
         </div>
